2b007cdae3ebabe262f61aeb11f7a2edeb4220a8
Pascal Irminger <11472778+pascalirminger@users.noreply.github.com>
2023-08-27 20:21:18 +0200

    End of section 13

--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useBidStore } from '@/hooks/useBidStore';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import Countdown, { zeroPad } from 'react-countdown';
 
@@ -7,7 +9,7 @@ type Props = {
     auctionEnd: string;
 };
 
-const renderer = ({ days, hours, minutes, seconds, completed }: {days: number, hours: number, minutes: number, seconds: number, completed: boolean}) => {
+const renderer = ({ days, hours, minutes, seconds, completed }: { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
     return (
         <div className={`
             border-2 border-white text-white py-1 px-2 rounded-lg flex justify-center
@@ -24,10 +26,19 @@ const renderer = ({ days, hours, minutes, seconds, completed }: {days: number, h
     )
 };
 
-export default function CountdownTimer({auctionEnd}: Props) {
+export default function CountdownTimer({ auctionEnd }: Props) {
+    const setOpen = useBidStore(state => state.setOpen);
+    const pathname = usePathname();
+
+    function auctionFinished() {
+        if (pathname.startsWith('/auctions/details')) {
+            setOpen(false);
+        }
+    }
+
     return (
         <div>
-            <Countdown date={auctionEnd} renderer={renderer} />
+            <Countdown date={auctionEnd} renderer={renderer} onComplete={auctionFinished} />
         </div>
     )
 }
